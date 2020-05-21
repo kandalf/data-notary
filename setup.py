c03f47fece841acc3ce5dd9a6b24c6b0e6232dff
Leonardo Mateo <leonardomateo@gmail.com>
2020-05-21 11:24:43 -0300

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "data-notary",
-    version = "0.1.2",
+    version = "0.2.0",
     description = "Simple data validators for Python",
     long_description = "README",
     long_description_content_type = "text/markdown",
